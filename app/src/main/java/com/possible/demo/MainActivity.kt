53cc64d50fb689b725321131aa08d90aa8ad5720
Armando Contreras <armando.contreras@wundermanthompson.com>
2020-03-24 15:01:21 -0600

    fixes build

--- app/src/main/java/com/possible/demo/MainActivity.kt
+++ app/src/main/java/com/possible/demo/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.possible.demo
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
